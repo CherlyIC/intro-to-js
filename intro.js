c25e071ef1f64d1be72359ee71ff7dc1c47113dd
CherlyIC <cherlyimfurankunda@gmail.com>
2026-05-16 14:20:06 +0200

    console log my age

---
+++ intro.js
@@ -0,0 +1,4 @@
+let name = "cherly"
+let age= 20
+let favProgrammingLanguage = "javascript"
+console.log("My name is " + name + ", I am " + age + " years old and my favorite programming language is " + favProgrammingLanguage + ".")
